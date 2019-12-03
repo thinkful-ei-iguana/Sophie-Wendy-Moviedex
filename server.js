89cf4be7bc3e3a61d37df0151f1f4a4804ecdf62
Wendy Bartos <wendyebartos@gmail.com>
2019-12-03 14:33:01 -0800

    validation token

--- server.js
+++ server.js
@@ -11,17 +11,19 @@ app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_BEARER_TOKEN;
   const authToken = req.get("Authorization");
 
-  const testToken = authToken.split(" ")[1];
-  console.log(testToken);
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "DENIED!" });
   }
   next();
 });
 
-app.get("/movie", validateBearerToken => {
-  res.send(MOVIEDEX);
+const validTypes = ["Animation", "Drama"];
+
+app.get("/movie", function handleGenreTypes(req, res) {
+  let response = MOVIEDEX[0].genre;
+  res.json(response);
 });
+
 const PORT = 8000;
 
 app.listen(PORT, () => {
