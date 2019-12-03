0f94e365ec3c186f6d83cca17fff085b276f400c
Wendy Bartos <wendyebartos@gmail.com>
2019-12-03 13:55:09 -0800

    validate token

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+/* eslint-disable quotes */
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-
+const MOVIEDEX = require("./movies.js");
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello, world!");
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_BEARER_TOKEN;
+  const authToken = req.get("Authorization");
+
+  const testToken = authToken.split(" ")[1];
+  console.log(testToken);
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "DENIED!" });
+  }
+  next();
 });
 
+app.get("/movie", validateBearerToken => {
+  res.send(MOVIEDEX);
+});
 const PORT = 8000;
 
 app.listen(PORT, () => {
